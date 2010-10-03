d43c016e2fe5a551aeab40b152e1095b2fbbfa7d
Alvar Lumberg <alvar.lumberg@gmail.com>
2010-10-03 10:55:00 +0300

    first

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using RatesComponent;
+
+namespace HelloWorld
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length > 0)
+      {
+        float amountInEEK = float.Parse(args[0]);
+        Console.WriteLine("Rates for amount EEK " + amountInEEK);
+        Rates rates = new Rates();
+        foreach (String key in rates.rates.Keys)
+        {
+          Console.WriteLine(key + " = " + rates.rates[key]);
+        }
+      }
+      else {
+        Console.WriteLine("Submit amount in EEK as argument!");
+      }
+    }
+  }
+}
---
+++ RatesComponent/Rates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RatesComponent
+{
+  public class Rates
+  {
+    public Dictionary<String, float> rates = new Dictionary<String, float>();    
+
+    public Rates()
+    {
+      WebRequest request = WebRequest.Create("http://www.eestipank.info/dynamic/erp/erp_csv.jsp");
+      WebResponse response = request.GetResponse();
+      StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
+      String date = reader.ReadLine();
+      String line;
+      while ((line = reader.ReadLine()) != null)
+      {
+        if (line.Contains(';'))
+        {
+          line = new Regex("[^A-Z;0-9,]").Replace(line, "");
+          Console.WriteLine(line);
+          string[] parts = line.Split(new char[] { ';' }, 2);
+          rates.Add(parts[0], float.Parse(parts[1]));
+        }
+      };
+      response.Close();
+    }
+  }
+}
